5ac83136ed137d5019a8926751364f36d731e765
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-09-26 09:34:18 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 const User = require("./models/user");
 
 app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -14,9 +15,35 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//  Feed API - GET /feed - get all the users from the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
-    console.log("Database connection established");
+    console.log("Database connection established...");
     app.listen(7777, () => {
       console.log("Server is successfully listening on port->7777...!");
     });
