9432541c941dd2a0f2d00dac6217836b654b510f
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-05 14:46:47 +0530

    routing created

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://rajesh:smart@newdevx.3slfz.mongodb.net/devx"
+    "mongodb+srv://rajesh:smart@devxapp.shgwr.mongodb.net/devx"
   );
 };
 
