1c05b11dce7ff4a443278e2812596332271947d9
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-06 17:05:47 +0530

    all done

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const cors = require("cors");
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credential: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type"],
   })
 );
 
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://rajesh:smart@devxapp.shgwr.mongodb.net/devx"
+    "mongodb+srv://rajesh:smart@devx.vkck9.mongodb.net/devx"
   );
 };
 
